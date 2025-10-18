7124d3e7ba0e428a4201532a579e541fe08a034a
Bleyle <Bennybleylle09@gmail.com>
2025-10-18 04:34:28 +0300

    Add files via upload

--- components/organiser-dashboard/EventsManagementPage.tsx
+++ components/organiser-dashboard/EventsManagementPage.tsx
@@ -657,8 +657,8 @@ export default function EventsManagementPage() {
                     </div>
                 ) : (
                     <div className={`${viewMode === 'grid'
-                        ? 'grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6'
-                        : 'space-y-6'
+                        ? 'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6'
+                        : 'space-y-4'
                         }`}>
                         {filteredEvents.map((event, index) => (
                             <EventCard
